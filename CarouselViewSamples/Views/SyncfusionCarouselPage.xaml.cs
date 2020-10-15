a8ae0d0f6e1863d7db63814befc7ce216acde9de
InquisitorJax <inquisitorjax@gmail.com>
2020-10-15 14:26:11 +0200

    update forms

---
+++ CarouselViewSamples/Views/SyncfusionCarouselPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using CarouselViewSamples.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselViewSamples.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SyncfusionCarouselPage : ContentPage
+	{
+		CarouselViewModel _viewModel;
+
+		public SyncfusionCarouselPage()
+		{
+			InitializeComponent();
+
+			BindingContext = _viewModel = new CarouselViewModel();
+		}
+
+		protected override async void OnAppearing()
+		{
+			base.OnAppearing();
+			await _viewModel.LoadItemsAsync();
+		}
+	}
+}
